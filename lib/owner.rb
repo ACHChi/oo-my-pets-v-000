960b2b1fdadeedd3c66c31d9110ba928fd342e05
Chao Hsuan Chi <allen0413@gmail.com>
2018-12-15 02:18:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :name, :cat, :dog, :fish
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @name = name
+    @species = species
+    @@all << self
+    @@count += 1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
 end
